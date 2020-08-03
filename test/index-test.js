8d5996dbcf2b2e15083c9ba81c2cbc37f33024d1
Patrick <pdunton@gmail.com>
2020-08-03 22:19:44 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -22,7 +22,7 @@ console.error("HALP!")
 
   it('calls console.log()', done => {
     const spy = expect.spyOn(console, 'log').andCallThrough()
-
+console.log("Logging")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
@@ -44,3 +44,4 @@ console.error("HALP!")
     })
   })
 })
+console.warn("warnings")
